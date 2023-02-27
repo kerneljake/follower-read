dc1c87be040c66805ca657314c82ad120e3a1e65
Jake Angerman <jangerman@yugabyte.com>
2023-02-27 16:12:06 -0500

    inner and outer try/cach

--- follower-read/Program.cs
+++ follower-read/Program.cs
@@ -10,51 +10,56 @@ namespace Yugabyte_CSharp_Demo
             NpgsqlConnection connection = new NpgsqlConnection(connStringBuilder);
             try
             {
-                connection.Open();
-
-                /* 
-                NpgsqlCommand empCreateCmd = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS employee (id int PRIMARY KEY, name varchar, age int, language varchar) SPLIT INTO 1 TABLETS;", connection);
-                empCreateCmd.ExecuteNonQuery();
-                Console.WriteLine("Created table Employee");
-
-                NpgsqlCommand empInsertCmd = new NpgsqlCommand("INSERT INTO employee (id, name, age, language) VALUES (1, 'John', 35, 'CSharp');", connection);
-                int numRows = empInsertCmd.ExecuteNonQuery();
-                Console.WriteLine("Inserted data (1, 'John', 35, 'CSharp')");
-                */
-
-                //NpgsqlTransaction transaction = connection.BeginTransaction();
-                //SET TRANSACTION READ ONLY;  // https://github.com/npgsql/npgsql/issues/867#issuecomment-157060944
-
-                NpgsqlCommand sessionCharacterisitcs = new NpgsqlCommand("SET SESSION CHARACTERISTICS AS TRANSACTION READ ONLY", connection);
-                sessionCharacterisitcs.ExecuteNonQuery();
-
-                NpgsqlCommand myQuery = new NpgsqlCommand("SELECT name, age, language FROM employee WHERE id = @EmployeeId", connection);
-                myQuery.Parameters.Add("@EmployeeId", YBNpgsqlTypes.NpgsqlDbType.Integer);
-                myQuery.Parameters["@EmployeeId"].Value = 1;
-
                 while (true)
                 {
-
-
-                    DateTime before = DateTime.Now;
-                    NpgsqlDataReader reader = myQuery.ExecuteReader();
-                    //transaction.Commit();
-                    DateTime after = DateTime.Now;
-                    TimeSpan timeSpan = after - before;
-
-                    Console.WriteLine("Query took {0}ms and returned:\nName\tAge\tLanguage", timeSpan.TotalMilliseconds);
-                    while (reader.Read())
+                    try
                     {
-                        Console.WriteLine("{0}\t{1}\t{2}", reader.GetString(0), reader.GetInt32(1), reader.GetString(2));
-                    }
-                    reader.Close();
+                        connection.Open();
 
-                    System.Threading.Thread.Sleep(1000);
-                }
+                        /* 
+                        NpgsqlCommand empCreateCmd = new NpgsqlCommand("CREATE TABLE IF NOT EXISTS employee (id int PRIMARY KEY, name varchar, age int, language varchar) SPLIT INTO 1 TABLETS;", connection);
+                        empCreateCmd.ExecuteNonQuery();
+                        Console.WriteLine("Created table Employee");
+
+                        NpgsqlCommand empInsertCmd = new NpgsqlCommand("INSERT INTO employee (id, name, age, language) VALUES (1, 'John', 35, 'CSharp');", connection);
+                        int numRows = empInsertCmd.ExecuteNonQuery();
+                        Console.WriteLine("Inserted data (1, 'John', 35, 'CSharp')");
+                        */
+
+                        NpgsqlCommand sessionCharacterisitcs = new NpgsqlCommand("SET SESSION CHARACTERISTICS AS TRANSACTION READ ONLY", connection);
+                        sessionCharacterisitcs.ExecuteNonQuery();
+
+                        NpgsqlCommand myQuery = new NpgsqlCommand("SELECT name, age, language FROM employee WHERE id = @EmployeeId", connection);
+                        myQuery.Parameters.Add("@EmployeeId", YBNpgsqlTypes.NpgsqlDbType.Integer);
+                        myQuery.Parameters["@EmployeeId"].Value = 1;
+
+                        while (true)
+                        {
+                            DateTime before = DateTime.Now;
+                            NpgsqlDataReader reader = myQuery.ExecuteReader();
+                            //transaction.Commit();
+                            DateTime after = DateTime.Now;
+                            TimeSpan timeSpan = after - before;
+
+                            Console.WriteLine("Query took {0}ms and returned:\nName\tAge\tLanguage", timeSpan.TotalMilliseconds);
+                            while (reader.Read())
+                            {
+                                Console.WriteLine("{0}\t{1}\t{2}", reader.GetString(0), reader.GetInt32(1), reader.GetString(2));
+                            }
+                            reader.Close();
+
+                            System.Threading.Thread.Sleep(1000);
+                        } /*while*/
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Inner catch: " + ex.Message);
+                    }
+                } /*while*/
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Failure: " + ex.Message);
+                Console.WriteLine("Outer catch: " + ex.Message);
             }
             finally
             {
